26e949a695b9a670a0e12385eef294b3f83e1e8f
Joona Suopuro <joona.suopuro@gmail.com>
2022-04-08 16:39:35 +0300

    Portti korjattu

---
+++ app.js
@@ -0,0 +1,67 @@
+// tehdään express app
+var express = require("express")
+var app = express()
+var db = require("./database.js")
+
+var bodyParser = require("body-parser")
+const res = require("express/lib/response")
+const req = require("express/lib/request")
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+var HTTP_PORT = process.env.PORT || 8080
+// Käynnistetään serveri
+app.listen(HTTP_PORT, () =>{
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+}
+)
+
+app.get("/", (req, res, next) =>{
+    res.json({"message":"OK"})
+})
+
+app.get("/api/measurements/", (req, res, next) =>{
+    var select = "SELECT * FROM mittaus;"
+
+    db.all(select, (err, rows) => {
+        if(err) {
+            res.status(400).json({"error":err.message})
+            return
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+    })
+})
+
+app.post("/api/measurements/", (req,res,next) =>{
+    var errors =[]
+    if(!req.body.numero) {
+        errors.push("Numero ei määritetty")
+    }
+    if (errors.length){
+        res.status(400).json({"error:": errors.join(",")})
+    }
+
+    var data = {
+        numero: req.body.numero
+    }
+
+    var insert = "INSERT INTO mittaus(numero, aika) VALUES (?,DATETIME(CURRENT_TIMESTAMP, \'localtime\'));"
+    var param = [data.numero]
+    db.run(insert, param, function(err, result){
+        if(err) {
+            res.status(400).json({"error":err.message})
+            console.log("sql error: %ERR%".replace("%ERR%", err.message))
+            return
+        }
+        res.json({
+            "message":"success",
+            "data":data,
+            "id":this.lastID
+        })
+
+
+    })
+})
